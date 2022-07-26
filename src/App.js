80d14b66d29c04b2b5e03a259c97303fffc57300
Patrick Merchant <patrickmerchant9@gmail.com>
2022-07-26 16:31:34 +0100

    tests apollo client query

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import graphQLAPI from "./db";
+import { useEffect } from "react";
+
 function App() {
+
+  // useEffect(() => {
+  //   const apiQuery = {
+  //     query: `
+  //     {
+  //       info 
+  //       feed {
+  //           id
+  //           userId
+  //           title
+  //           body
+  //           }
+  //     }
+  //     `,
+  //   };
+
+  //   fetch(graphQLAPI.baseURL, {
+  //     method: "POST",
+  //     headers: graphQLAPI.headers,
+  //     body: JSON.stringify(apiQuery),
+  //   })
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     console.log(data);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+  // });
+
   return (
     <div className="App container mt-5">
-      <h1 className="text-primary"><i className="bi bi-diagram-2-fill"></i>Repos</h1>
+      <h1 className="text-primary"><i className="bi bi-diagram-2-fill"></i>hello world</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './custom.scss';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8081/graphql',
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query getPosts {
+    feed {
+        id
+        userId
+        title
+        body
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result.data.feed));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

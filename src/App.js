bdcf27eeb9aa4b17b43d1871711b8b9208975171
Patrick Merchant <patrickmerchant9@gmail.com>
2022-07-26 16:38:51 +0100

    imports useQuery to App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { useQuery, gql } from '@apollo/client';
+
+function App() {
+
+  return (
+    <div className="App container mt-5">
+      <h1 className="text-primary"><i className="bi bi-diagram-2-fill"></i>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
